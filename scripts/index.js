63c53c6e6430138221b0953c5e1ccf53570f9313
Denis Baranov <finedend@gmail.com>
2025-04-12 09:13:39 +0300

    Добавил загрузку карточек на сайте

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,41 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+// DOM узлы
+const placesList = document.querySelector('.places__list');
+// Функция создания карточки
+function createCard(cardData, deleteCallBack) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    const likeButton = cardElement.querySelector('.card__like-button');
+    
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
 
-// @todo: DOM узлы
+    deleteButton.addEventListener('click', () => {
+        deleteCallBack(cardElement);
+    });
 
-// @todo: Функция создания карточки
+    likeButton.addEventListener('click', () => {
+        likeButton.classList.toggle('card__like-button_is-active');
+    });
 
-// @todo: Функция удаления карточки
+    cardImage.addEventListener('click', () => {
+        console.log('Открыть изображение: ', cardData.link);
+    });
 
-// @todo: Вывести карточки на страницу
+    
+
+    return cardElement;
+}
+// Функция удаления карточки
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+// Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+    const newCard = createCard(cardData, deleteCard);
+    placesList.appendChild(newCard);
+});
